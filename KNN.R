a114aef6f34b9a9803dc88ff0f9588dc86d40257
peilivanov <45008165+peilivanov@users.noreply.github.com>
2018-11-22 08:16:40 +0200

    Update KNN.R

--- KNN.R
+++ KNN.R
@@ -1,48 +1,54 @@
-## Евклидово расстояние
+
 euclideanDistance <- function(u, v)
 {
   sqrt(sum((u - v)^2))
 }
 
-## Сортируем объекты согласно расстояния до объекта z
+
 sortObjectsByDist <- function(xl, z, metricFunction =
                                 euclideanDistance)
 {
   l <- dim(xl)[1]
   n <- dim(xl)[2] - 1
-  ## Создаём матрицу расстояний
   distances <- matrix(NA, l, 2)
   for (i in 1:l)
   {
     distances[i, ] <- c(i, metricFunction(xl[i, 1:n], z))
   }
-  ## Сортируем
+
   orderedXl <- xl[order(distances[, 2]), ]
   return (orderedXl);
 }
-## Применяем метод kNN
+
 kNN <- function(xl, z, k)
 {
-  ## Сортируем выборку согласно классифицируемого
-  объекта
+
   orderedXl <- sortObjectsByDist(xl, z)
   n <- dim(orderedXl)[2] - 1
-  ## Получаем классы первых k соседей
   classes <- orderedXl[1:k, n + 1]
-  ## Составляем таблицу встречаемости каждого класса
-  22
   counts <- table(classes)
-  ## Находим класс, который доминирует среди первых k
-  соседей
   class <- names(which.max(counts))
   return (class)
 }
-## Рисуем выборку
+map = function(k){
+text <- paste("Map classificaton for kNN with k = ", k)
+plot(iris[, 3:4],main=text, pch = 21, bg = colors[xl$Species], col = colors[xl$Species],asp='1')
+for(i in seq(0, 7, 0.1)){
+
+for(j in seq(0,3,0.08)){
+
+z <- c(i, j)
+orderedXl <- sortObjectByDist(xl, z)
+class <- knn(xl, z,k,orderedXl)
+points(z[1], z[2], pch = 1, bg = colors[class],col=colors[class])
+
+}
+}
+}
 colors <- c("setosa" = "red", "versicolor" = "green3",
             "virginica" = "blue")
 plot(iris[, 3:4], pch = 21, bg = colors[iris$Species], col
      = colors[iris$Species], asp = 1)
-## Классификация одного заданного объекта
 z <- c(2.7, 1)
 xl <- iris[, 3:5]
 class <- kNN(xl, z, k=6)
